a5ce5c744aa0ad50b0a55733338accf871f418f0
azril 019 <azril.muhammad0001@gmail.com>
2025-04-30 23:39:11 +0700

    Enhance Navbar component styling and improve user interaction; update logo size and add transition effects to links

--- Clients/src/components/Navbar.jsx
+++ Clients/src/components/Navbar.jsx
@@ -5,36 +5,48 @@ export default function Navbar() {
 
   const handleLogout = () => {
     localStorage.removeItem("access_token");
-
     navigate("/");
   };
+
   return (
-    <nav className="bg-blue-600 p-4 text-white shadow-md">
+    <nav className="custom-nav p-4 text-white shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <Link to={"/home"}>
-          <img style={{ width: "20%" }} src="/Logo.png" alt="Logo" />
+          <img style={{ width: "25%" }} src="/Logo.png" alt="Logo" />
         </Link>
         <ul className="flex space-x-4">
           <li>
-            <a href="/home/profile" className="hover:underline text-white">
-              👤User
+            <a
+              href="/home/profile"
+              className="hover:text-indigo-200 transition-colors duration-200 text-white"
+            >
+              👤 User
             </a>
           </li>
           <li>
-            <a href="/home/preference" className="hover:underline text-white">
-              💼Preferences
+            <a
+              href="/home/preference"
+              className="hover:text-indigo-200 transition-colors duration-200 text-white"
+            >
+              💼 Preferences
             </a>
           </li>
           <li>
             <button
               onClick={handleLogout}
-              className="hover:underline text-white"
+              className="hover:text-indigo-200 transition-colors duration-200 text-white"
             >
-              🫂Logout
+              🫂 Logout
             </button>
           </li>
         </ul>
       </div>
+
+      <style jsx>{`
+        .custom-nav {
+          background-color: #6a64f1;
+        }
+      `}</style>
     </nav>
   );
 }
