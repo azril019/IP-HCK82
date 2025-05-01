b55a6b01876548c73ef19558c569a25530ad950c
azril 019 <azril.muhammad0001@gmail.com>
2025-05-01 21:06:11 +0700

    Remove Firebase configuration files and update AppLayout to enforce authentication

--- Clients/src/App.jsx
+++ Clients/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import AppLayout from "./Layouts/AppLayout";
+import AppLayout from "./layouts/AppLayout";
 import JobCard from "./components/JobCard";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
